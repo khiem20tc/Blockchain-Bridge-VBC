6a4dc6a832b24cc0772e57274926fa7cf97e871f
Luong An Khang <khangluong2004@gmail.com>
2022-12-01 15:58:11 +0700

    Clean code by separate functions

--- automation-selenium/test/common.js
+++ automation-selenium/test/common.js
@@ -1,21 +1,7 @@
 const { By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
-
-let WaitAndClick = async(driver, method, locator) => {
-    await driver.wait(until.elementLocated(By[method](locator)), 25000);
-    await driver.findElement(By[method](locator)).click();
-  }
-  
-let WaitAndSelect = async(driver, method_select, locator_select, method_opt, locator_opt) => {
-    await driver.wait(until.elementLocated(By[method_select](locator_select)), 15000);
-    let select_element = await driver.findElement(By[method_select](locator_select));
-    await driver.executeScript("arguments[0].scrollIntoView()", select_element);
-    await driver.wait(until.elementIsVisible(await driver.findElement(By[method_select](locator_select))), 5000);
-    await driver.executeScript("arguments[0].click()", select_element);
-    await driver.wait(until.elementIsEnabled(await select_element.findElement(By[method_opt](locator_opt))), 25000);
-    await select_element.findElement(By[method_opt](locator_opt)).click();
-  }
+const {WaitAndClick, WaitAndSelect} = require('../utils/index');
 
 let LockNative = async(address, driver, handle, Login, ConfirmMetamask, ConnectMetamask, SwitchNetwork, network) => {
     // Test name: Lock Native token 1st time

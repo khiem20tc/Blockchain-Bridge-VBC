213c830cf30dc00c7086d208dc67334b898921a7
Luong An Khang <khangluong2004@gmail.com>
2022-12-07 20:05:38 +0700

    Update React Docker file

--- signature_server/server.js
+++ signature_server/server.js
@@ -1,5 +1,5 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
